6dd93a50464a2983f4b8b4f66eb07fa01ecf49d3
rainikita <158440602+rainikita@users.noreply.github.com>
2024-10-21 12:55:05 +0530

    Updating the DB

---
+++ db/V3__create_schema_vpa.sql
@@ -0,0 +1,8 @@
+ALTER TYPE dmsr.vevt RENAME TO vevts;
+
+ALTER TABLE dmsr.vpa
+ALTER COLUMN vevt TYPE dmsr.vevts USING vevt::dmsr.vevts;
+
+
+ALTER TABLE dmsr.vpa_v
+ALTER COLUMN vevt TYPE dmsr.vevts USING vevt::dmsr.vevts;
